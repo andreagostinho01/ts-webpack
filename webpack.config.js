1254564fc6745cd7f3e4186e23aface1d6341f21
andre <andre.carlos.agostinho@gmail.com>
2021-01-07 13:52:32 -0300

    lesson 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,27 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.ts',
-    module: {
-      rules: [
-          {
-            test: /\.tsx?$/,
-            use: 'ts-loader',
-            include: [path.resolve(__dirname, 'src')],
-            exclude: /node_modules/
-          }
-      ]
-    },
-    resolve: {
-      extensions: ['.ts', '.tsx', '.js']
-    },
-    output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'public')
-    },
-    mode: 'development'
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        include: [path.resolve(__dirname, 'src')],
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  devServer: {
+    publicPath: '/',
+    contentBase: path.join(__dirname, 'public')
+  },
+  mode: 'development'
 }
